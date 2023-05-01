2154f69ac8e734d10da931c6b801b6aea88649a2
Torra Tran <pandapunch@Torras-MacBook-Air.local>
2023-05-01 16:58:47 -0700

    done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//If the customer has a coupon, the customer will receive a $10 discount after the subscription discount has been calculated.
+//Copy and paste the following customer objects into a new index.js file.
 
 const timmy = {
   prescription: "acetaminophen",
@@ -28,13 +28,19 @@ function sumOfPrice(prefill, refill) {
   return prefill * refill;
 }
 
+//If a customer has a subscription, the customer will receive a 25% discount after the refill total has been calculated.
+
 function subscription(total) {
   return total - total * 0.25;
 }
 
+//If the customer has a coupon, the customer will receive a $10 discount after the subscription discount has been calculated.
+
 function coupon(total) {
     return total >= 10 ? total - 10 : total;
 }
+
+//At the end of the script, you should return and log the string "Your grand total is ${finalAmount}." 
 
 function grandTotal (customer){
     let refill = sumOfPrice(customer.pricePerRefill, customer.refills);
